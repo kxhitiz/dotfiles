b1c65206c6bdfd3dc0e552c33d8bcbe6f267a52b
kxhitiz <nirvanagrg@gmail.com>
2024-02-04 21:49:47 -0700

    Update nvim confg

--- nvim/lua/kxhitiz/plugins/indent_blankline.lua
+++ nvim/lua/kxhitiz/plugins/indent_blankline.lua
@@ -3,13 +3,14 @@ return {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
     opts = {},
-    config = function()
-      require("ibl").setup()
+    config = true,
+    -- function()
+      -- require("ibl").setup()
       --   require("indent_blankline").setup({
       --     -- for example, context is off by default, use this to turn it on
       --     show_current_context = true,
       --     show_current_context_start = true,
       --   })
-    end,
+    -- end,
   },
 }
---
+++ nvim/lua/kxhitiz/plugins/nvim_ufo.lua
@@ -0,0 +1,45 @@
+return {
+  {
+    "kevinhwang91/nvim-ufo",
+    event = "BufRead",
+    dependencies = {
+      { "kevinhwang91/promise-async" },
+      {
+        "luukvbaal/statuscol.nvim",
+        config = function()
+          local builtin = require("statuscol.builtin")
+          require("statuscol").setup({
+            -- foldfunc = "builtin",
+            -- setopt = true,
+            relculright = true,
+            segments = {
+              { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+              { text = { "%s" }, click = "v:lua.ScSa" },
+              { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+            },
+          })
+        end,
+      },
+    },
+    keys = {
+      { "zo", function() require("ufo").openAllFolds() end },
+      { "zc", function() require("ufo").closeAllFolds() end },
+      { "zk", function()
+        local winid = require('ufo').peekFoldedLinesUnderCursor()
+        if not winid then
+          vim.lsp.buf.hover()
+        end
+      end }
+    },
+    config = function()
+      -- Fold options
+      vim.o.fillchars = [[eob: ,fold: ,foldsep: ]]
+      vim.o.foldcolumn = "1" -- '0' is not bad
+      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+
+      require("ufo").setup()
+    end,
+  }
+}
